0b2b10946fb1c59e9470f52a0d1b7ea954344ddd
Marcos Oliveira Soares <marcosoliveirasoares94@gmail.com>
2024-04-26 00:32:38 -0300

    Adjusting the migration to get the table name from the model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Modules\Auth\Models\User;
 
 return new class() extends Migration
 {
@@ -13,18 +14,20 @@ return new class() extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->uuid();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->boolean('active')->default(true);
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        if (! Schema::hasTable(User::getModelTable())) {
+            Schema::create(User::getModelTable(), function (Blueprint $table) {
+                $table->id();
+                $table->uuid();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->string('role');
+                $table->boolean('active')->default(true);
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
@@ -32,6 +35,6 @@ return new class() extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists(User::getModelTable());
     }
 };
